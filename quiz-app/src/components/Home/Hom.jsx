68d2ffb0265a61144110532957b6e7dc65990380
karantech <karanrana3095@gmail.com>
2023-09-26 17:13:47 +0530

    finalcommit:

--- quiz-app/src/components/Home/Hom.jsx
+++ quiz-app/src/components/Home/Hom.jsx
@@ -6,15 +6,15 @@ const Hom = () => {
     <div> 
     {/* Sidebar  */}
         <div className="row">
-        <div className="col-md-3">
+        <div className="col-md-2">
             <Sidebar/>
         </div>
         {/* Main */}
-    <div className="col-md-6 mt-5">
+    <div className="col-md-7 mt-5">
     <div className="container my-">
     <div className="d-flex">
         <h3 className="p-2 flex-fill">Create User</h3>
-        <img src="Vector.png" width="60" className="p-2 img-fluid rounded-circle"/>
+        <img src="Vector.png" width="60" className="p-2 img-fluid rounded-circle border border-primary"/>
         <img src="picture.png" className="p-2 img-fluid rounded-circle"/>
         </div>
 <div className="container mt-6">
@@ -54,7 +54,7 @@ const Hom = () => {
 </div>
  </div>
  {/* Rightbar */}
- <div className="col-md-3 mr-5 ">
+ <div className="col-md-3 mr-5 widt ">
 <div className="d-flex flex-column mb-3 mt-4 size2 ">
 <div className="p-2 set">Total Users</div>
 <div className="p-2 set">1,0000</div>
@@ -64,17 +64,17 @@ const Hom = () => {
         <div className="p-4 w-100">Users</div>
         <button className="mb-3 btn btn-white color3 flex-shrink-1" data-toggle="modal" data-target="#exampleModal" >+</button>
         </div>
-        <div className="d-flex mb-3">
+        <div className="d-flex mb-3 border">
         <img src="picture2.png" width="60" className="p-2 img-fluid rounded-circle"/>
         <div className="p-3 bold">Name</div>
   
         </div>
-        <div className="d-flex mb-3">
+        <div className="d-flex mb-3 border">
         <img src="picture2.png" width="60" className="p-2 img-fluid rounded-circle"/>
         <div className="p-3 bold" >Name</div>
   
         </div>
-        <div className="d-flex mb-3">
+        <div className="d-flex mb-3 border">
         <img src="picture2.png" width="60" className="p-2 img-fluid rounded-circle"/>
         <div className="p-3 bold">Name</div>
   
